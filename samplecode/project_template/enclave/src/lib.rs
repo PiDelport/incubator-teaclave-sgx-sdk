2d08febcb7cc9e7464750d6fd608fe753082f734
Pi Delport <pjdelport@gmail.com>
2021-08-10 16:52:40 +0200

    remove crate_name / crate_type from lib.rs

--- samplecode/project_template/enclave/src/lib.rs
+++ samplecode/project_template/enclave/src/lib.rs
@@ -15,8 +15,6 @@
 // specific language governing permissions and limitations
 // under the License..
 
-#![crate_name = "sample"]
-#![crate_type = "staticlib"]
 #![cfg_attr(not(target_env = "sgx"), no_std)]
 #![cfg_attr(target_env = "sgx", feature(rustc_private))]
 
